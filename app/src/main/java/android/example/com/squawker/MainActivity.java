41148f79b0424e5d9212d154a73265dc6a462be1
James Kim <jhlink@users.noreply.github.com>
2018-11-01 14:45:35 -0400

    task[1-2]: Create SquawkerFirebaseInstanceIdService class and implement onRefreshToken method

--- app/src/main/java/android/example/com/squawker/MainActivity.java
+++ app/src/main/java/android/example/com/squawker/MainActivity.java
@@ -95,8 +95,8 @@ public class MainActivity extends AppCompatActivity implements
             Log.d(LOG_TAG, "Contains: " + extras.getString("test"));
         }
 
-        // TODO (1) Make a new package for your FCM service classes called "fcm"
-            // TODO (2) Create a new Service class that extends FirebaseInstanceIdService.
+        // DONE (1) Make a new package for your FCM service classes called "fcm"
+            // DONE (2) Create a new Service class that extends FirebaseInstanceIdService.
             // You'll need to implement the onTokenRefresh method. Simply have it print out
             // the new token.
         // TODO (3) Here, in MainActivity, get a token using FirebaseInstanceId.getInstance().getToken()
